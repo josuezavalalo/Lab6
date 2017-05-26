2daf6f00d698ba7c678cc7ea5ec3a5217e9defc7
wilmer soianon <josuezavala@unitec.edu>
2017-05-26 13:03:08 -0600

    nuevo projecto

---
+++ Lab6WilmerJosue11511242/src/lab6wilmerjosue11511242/Lab6WilmerJosue11511242.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab6wilmerjosue11511242;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class Lab6WilmerJosue11511242 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
